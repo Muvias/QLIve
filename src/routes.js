76efc719c8faed323c198d6c69c686dcc69ec67d
Vinicius Perazza <vi_guepardo@hotmail.com>
2021-09-08 23:44:02 -0300

    MyPortifolio

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+const { response } = require('express')
+const express = require('express')
+
+const routes = express.Router()
+
+routes.get('/', (req, res) => res.render("index"))
+routes.get('/room', (req, res) => res.render("room"))
+routes.get('/create-pass', (req, res) => res.render("create-pass"))
+
+module.exports = routes
